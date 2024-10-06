d585043f74ae97979d1000cc3e9cebd58d31198e
LEONID <312agapov@gmai.com>
2024-10-06 11:57:58 +0400

    first commit

---
+++ src/main/java/ru/mazdarunner/PerekupHelper/PerekupHelperApplication.java
@@ -0,0 +1,13 @@
+package ru.mazdarunner.PerekupHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerekupHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerekupHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mazdarunner/PerekupHelper/PerekupHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mazdarunner.PerekupHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerekupHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
